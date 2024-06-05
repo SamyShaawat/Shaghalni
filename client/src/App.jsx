578e011f8808a5b3ca9d00ad4b9688716e34a99c
SamyShaawat <samy.shaawat@gmail.com>
2024-06-06 00:17:23 +0300

    setup enviroment

---
+++ client/src/App.jsx
@@ -0,0 +1,14 @@
+import { Outlet } from "react-router-dom";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <nav>Navbar</nav>
+      <Outlet />
+      <footer>Footer</footer>
+    </>
+  );
+}
+
+export default App;
---
+++ client/src/Pages/Home.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+const Home = () => {
+  return <div className="text-blue">Home page </div>;
+};
+
+export default Home;
